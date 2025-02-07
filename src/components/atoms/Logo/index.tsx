8788ad67e55765dbdf794528cbfbffed1c0ae05a
Cleyton-1995 <cleytoncosta.developer@gmail.com>
2025-02-07 08:08:55 -0300

    fix: update module declaration for PNG

--- src/components/atoms/Logo/index.tsx
+++ src/components/atoms/Logo/index.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { Image, StyleProp, ImageStyle, View } from "react-native";
 
+import LogoImage from "../../../assets/logo.png";
+
 interface LogoProps {
   style?: StyleProp<ImageStyle>;
 }
 export function Logo({ style }: LogoProps) {
-  return (
-      <Image style={style} source={require("../../../assets/logo.png")} />
-  );
+  return <Image style={style} source={LogoImage} />;
 }
