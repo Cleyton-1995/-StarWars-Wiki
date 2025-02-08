94e67e3fe2edbf5b684503506e3ec6657e84dd0c
Cleyton-1995 <cleytoncosta.developer@gmail.com>
2025-02-08 10:17:44 -0300

    fix: update styling

--- src/components/molecules/Button/styles.ts
+++ src/components/molecules/Button/styles.ts
@@ -5,8 +5,6 @@ export const styles = StyleSheet.create({
   button: {
     alignItems: "center",
     justifyContent: "center",
-    paddingHorizontal: theme.metrics.px(12),
-    paddingVertical: theme.metrics.px(8),
     borderRadius: theme.metrics.px(8),
     gap: theme.metrics.px(6),
     backgroundColor: theme.colors.dark, // Padrão
@@ -14,7 +12,6 @@ export const styles = StyleSheet.create({
 
   buttonText: {
     color: theme.colors.white,
-    fontSize: theme.metrics.px(16),
     fontFamily: theme.fonts.bold,
   },
 });
